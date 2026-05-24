1f0bd04ee4b529a81e6386ae868d98073af95256
Tangwenjun910 <461092733@qq.com>
2026-05-24 23:22:20 +0800

    chore: remove unused Item import from Config

--- src/main/java/com/zonlong/beloong/Config.java
+++ src/main/java/com/zonlong/beloong/Config.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
