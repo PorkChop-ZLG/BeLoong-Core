8a5fcae6a9d64373d28ceeeebacbda328b5ff408
PorkChop_ZLG <2571692644@qq.com>
2026-05-29 20:15:27 +0800

    细节优化

--- src/main/java/com/zonlong/beloong/mixin/TreasureBlockMixin.java
+++ src/main/java/com/zonlong/beloong/mixin/TreasureBlockMixin.java
@@ -5,7 +5,6 @@ import com.zonlong.beloong.Config;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
