ce49757562e6aab7e1b8cad5f7bda06c54fc4b70
PorkChop_ZLG <2571692644@qq.com>
2026-05-31 00:17:47 +0800

    feat: register tp_loong_palace effect with AbilityEntityEffect registry

---
+++ src/main/java/com/zonlong/beloong/ability/AbilityEffectRegistry.java
@@ -0,0 +1,23 @@
+package com.zonlong.beloong.ability;
+
+import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.entity_effects.AbilityEntityEffect;
+import com.zonlong.beloong.BeLoongCore;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.registries.RegisterEvent;
+
+@EventBusSubscriber(modid = BeLoongCore.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class AbilityEffectRegistry {
+
+    private AbilityEffectRegistry() {}
+
+    @SubscribeEvent
+    static void registerEntityEffects(final RegisterEvent event) {
+        if (event.getRegistry() == AbilityEntityEffect.REGISTRY) {
+            event.register(AbilityEntityEffect.REGISTRY_KEY,
+                    ResourceLocation.fromNamespaceAndPath(BeLoongCore.MODID, "tp_loong_palace"),
+                    () -> TpLoongPalaceEffect.CODEC);
+        }
+    }
+}
