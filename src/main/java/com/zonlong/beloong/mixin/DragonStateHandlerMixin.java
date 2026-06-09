f5df45ac9ed10fc4595eb622d82aa5dcb0ce948e
PorkChop_ZLG <2571692644@qq.com>
2026-06-10 00:27:28 +0800

    0.4.1：默认不开启碾压，改为mixin实现，修复hotai导致的兼容性问题

---
+++ src/main/java/com/zonlong/beloong/mixin/DragonStateHandlerMixin.java
@@ -0,0 +1,20 @@
+package com.zonlong.beloong.mixin;
+
+import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = DragonStateHandler.class, remap = false)
+public class DragonStateHandlerMixin {
+
+    @Shadow
+    public DragonStateHandler.LargeDragonDestruction largeDragonDestruction;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void disableDestructionByDefault(CallbackInfo ci) {
+        this.largeDragonDestruction = DragonStateHandler.LargeDragonDestruction.DISABLED;
+    }
+}
